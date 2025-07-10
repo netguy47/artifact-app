dfb1946d4081fc761a5dc2f5df4abca13bc58424
netguy47 <donaldwoods@live.com>
2025-07-10 17:01:07 -0500

    fix: update Ollama import for deployment

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.llms import Ollama
+from langchain_community.llms import Ollama
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
